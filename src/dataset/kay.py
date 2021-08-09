50893453998c1a4480137af7cd235bd27f8c54f8
Akshaj Verma <akshajverma7@gmail.com>
2021-08-09 19:25:59 +0530

    Added script for loading dataset.

--- src/dataset/kay.py
+++ src/dataset/kay.py
@@ -2,8 +2,13 @@
 Functions to access the dataset.
 """
 
+import os
+import requests
+import numpy as np
+from tqdm import tqdm
 
-def download_dataset(data_path: str):
+
+def download_data(data_path: str):
     """Download the dataset.
 
     Parameters
@@ -12,8 +17,30 @@ def download_dataset(data_path: str):
         Path to save the dataset.
 
     """
-    a = "dataset"
-    return a
+    fnames = ["kay_labels.npy", "kay_labels_val.npy", "kay_images.npz"]
+    fnames = [f"{data_path}" + file for file in fnames]
+
+    urls = [
+        "https://osf.io/r638s/download",
+        "https://osf.io/yqb3e/download",
+        "https://osf.io/ymnjv/download",
+    ]
+
+    if len(os.listdir(data_path)) == len(fnames):
+        print("The files are already downloaded.")
+    else:
+        for i in tqdm(range(len(urls))):
+            if not os.path.isfile(fnames[i]):
+                try:
+                    r = requests.get(urls[i])
+                except requests.ConnectionError:
+                    print("Download failed.")
+                else:
+                    if r.status_code != requests.codes.ok:
+                        print("Download failed.")
+                    else:
+                        with open(fnames[i], "wb") as fid:
+                            fid.write(r.content)
 
 
 def load_dataset(data_path: str):
@@ -25,5 +52,14 @@ def load_dataset(data_path: str):
         Path to load the dataset from.
 
     """
-    a = "dataset"
-    return a
+    # download data if not already present.
+    if len(os.listdir(data_path)) == 0:
+        download_data(data_path)
+
+    with np.load(f"{data_path}/kay_images.npz") as dobj:
+        img_dict = dict(**dobj)
+
+    train_labels = np.load(f"{data_path}/kay_labels.npy")
+    val_labels = np.load(f"{data_path}/kay_labels_val.npy")
+
+    return img_dict, train_labels, val_labels
--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -2,4 +2,4 @@ from src.dataset.kay import load_dataset
 
 
 def test_load_dataset():
-    assert load_dataset(data_path="./data") == "dataset"
+    assert len(load_dataset(data_path="./data/")) == 3
