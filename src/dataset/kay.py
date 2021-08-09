5eb6a73ae1beffa217c8c383b05fb2411f1244a1
Akshaj Verma <akshajverma7@gmail.com>
2021-08-09 23:01:23 +0530

    Return label transpose.

--- src/dataset/kay.py
+++ src/dataset/kay.py
@@ -62,7 +62,7 @@ def load_dataset(data_path: str):
     train_labels = np.load(f"{data_path}/kay_labels.npy")
     val_labels = np.load(f"{data_path}/kay_labels_val.npy")
 
-    all_data["train_labels"] = train_labels
-    all_data["test_labels"] = val_labels
+    all_data["train_labels"] = train_labels.T
+    all_data["test_labels"] = val_labels.T
 
     return all_data
