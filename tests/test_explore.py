461b67014483f1d98fd489219a263866928134c8
Akshaj Verma <akshajverma7@gmail.com>
2021-08-10 17:00:04 +0530

    Updated test file structure.

--- tests/test_explore.py
+++ tests/test_explore.py
@@ -20,14 +20,24 @@ for k in all_data_info.keys():
     dummy_data[k] = np.random.random_sample(size=(all_data_info[k]))
 
 
-def test_plot_hierarchical_labels():
+class TestSummary:
+    def test_plot_hierarchical_labels(self):
+        # label_level can only be one of - 0, 1, 2, 3
+        with pytest.raises(ValueError):
+            plot_hierarchical_labels(all_data=dict(), label_level=5)
 
-    # label_level can only be one of - 0, 1, 2, 3
-    with pytest.raises(ValueError):
-        plot_hierarchical_labels(all_data=dict(), label_level=5)
+        # shape of output must be 1750
+        assert (
+            len(
+                plot_hierarchical_labels(all_data=dummy_data, label_level=0, plot=False)
+            )
+            == 1750
+        )
 
-    # shape of output must be 1750
-    assert (
-        len(plot_hierarchical_labels(all_data=dummy_data, label_level=0, plot=False))
-        == 1750
-    )
+        # type of output must by np.ndarray
+        assert (
+            type(
+                plot_hierarchical_labels(all_data=dummy_data, label_level=0, plot=False)
+            )
+            == np.ndarray
+        )
