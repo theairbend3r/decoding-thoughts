5bd62e1912ffeae6a877ac6f887b420326309458
Akshaj Verma <akshajverma7@gmail.com>
2021-08-12 20:56:58 +0530

    Added torchinfo.

--- src/ml/model.py
+++ src/ml/model.py
@@ -13,19 +13,25 @@ class StimulusClassifier(nn.Module):
     def __init__(self, num_channel, num_classes):
         super(StimulusClassifier, self).__init__()
 
-        self.block1 = self.conv_block(
-            c_in=num_channel, c_out=32, kernel_size=3, stride=1, padding=1
+        self.block1 = nn.Sequential(
+            self.conv_block(
+                c_in=num_channel, c_out=8, kernel_size=3, stride=1, padding=1
+            ),
+            self.conv_block(c_in=8, c_out=8, kernel_size=3, stride=1, padding=1),
+            self.conv_block(c_in=8, c_out=8, kernel_size=3, stride=1, padding=1),
+            self.conv_block(c_in=8, c_out=8, kernel_size=3, stride=1, padding=1),
+            self.conv_block(c_in=8, c_out=16, kernel_size=3, stride=1, padding=1),
         )
         self.block2 = self.conv_block(
-            c_in=32, c_out=64, kernel_size=3, stride=1, padding=1
+            c_in=16, c_out=32, kernel_size=3, stride=1, padding=1
         )
         self.block3 = self.conv_block(
-            c_in=64, c_out=128, kernel_size=3, stride=1, padding=1
+            c_in=32, c_out=64, kernel_size=3, stride=1, padding=1
         )
         self.block4 = self.conv_block(
-            c_in=128, c_out=64, kernel_size=3, stride=1, padding=1
+            c_in=64, c_out=128, kernel_size=3, stride=1, padding=1
         )
-        self.fc = nn.Linear(in_features=4096, out_features=num_classes)
+        self.fc = nn.Linear(in_features=8 * 8 * 128, out_features=num_classes)
 
         self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
 
@@ -52,4 +58,13 @@ class StimulusClassifier(nn.Module):
             nn.Conv2d(in_channels=c_in, out_channels=c_out, **kwargs),
             nn.BatchNorm2d(num_features=c_out),
             nn.ReLU(),
+            nn.Dropout2d(p=0.5),
         )
+
+
+if __name__ == "__main__":
+    from torchinfo import summary
+
+    model = StimulusClassifier(num_channel=3, num_classes=5)
+    batch_size = 16
+    print(summary(model, input_size=(batch_size, 3, 128, 128)))
