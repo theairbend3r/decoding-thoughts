18d3a932c938129a12df5748dc2fb75a51d1d5b1
Akshaj Verma <akshajverma7@gmail.com>
2021-08-15 22:00:19 +0530

    Updated model and notebook.

--- src/ml/__init__.py
+++ src/ml/__init__.py
@@ -3,14 +3,19 @@ from albumentations.pytorch import ToTensorV2
 
 
 class StimulusClassifierConfig:
+    """
+    Configuration file for the stimulus classifier.
+    """
+
     def __init__(self):
         self.class_ignore_list = ["person", "fungus", "plant"]
         self.label_level = 0
         self.validation_dataset_size = 0.2
         self.model_input_num_channel = 3
-        self.epochs = 2
+        self.epochs = 20
         self.learning_rate = 0.0001
         self.batch_size = 128
+        self.latent_emb_size = 64
         self.og_transform = A.Compose(
             [A.Normalize(mean=(0, 0, 0), std=(1, 1, 1)), ToTensorV2()]
         )
@@ -32,11 +37,16 @@ class StimulusClassifierConfig:
 
 
 class FMRIClassifierConfig:
+    """
+    Configuration file for the fMRI classifier.
+    """
+
     def __init__(self):
         self.class_ignore_list = ["person", "fungus", "plant"]
         self.label_level = 0
         self.validation_dataset_size = 0.2
-        self.roi_select_list = [1, 2, 3]
-        self.epochs = 2
+        self.roi_select_list = [1, 2, 3, 4, 5, 6, 7]
+        self.epochs = 20
         self.learning_rate = 0.0001
         self.batch_size = 128
+        self.latent_emb_size = 64
--- src/ml/model.py
+++ src/ml/model.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from src.ml import StimulusClassifierConfig, FMRIClassifierConfig
 
 
 class StimulusClassifier(nn.Module):
@@ -8,58 +9,36 @@ class StimulusClassifier(nn.Module):
     def __init__(self, num_channel, num_classes):
         super(StimulusClassifier, self).__init__()
 
-        self.block1 = nn.Sequential(
+        self.config = StimulusClassifierConfig()
+
+        self.feature_extractor = nn.Sequential(
             self.conv_block(
-                c_in=num_channel, c_out=8, kernel_size=3, stride=1, padding=1
+                c_in=num_channel, c_out=16, kernel_size=3, stride=1, padding=1
             ),
-            self.conv_block(c_in=8, c_out=8, kernel_size=3, stride=1, padding=1),
-            self.conv_block(c_in=8, c_out=8, kernel_size=3, stride=1, padding=1),
-            self.conv_block(c_in=8, c_out=8, kernel_size=3, stride=1, padding=1),
-            self.conv_block(c_in=8, c_out=16, kernel_size=3, stride=1, padding=1),
-        )
-
-        self.block2 = nn.Sequential(
-            self.conv_block(c_in=16, c_out=16, kernel_size=3, stride=1, padding=1),
-            self.conv_block(c_in=16, c_out=16, kernel_size=3, stride=1, padding=1),
-            self.conv_block(c_in=16, c_out=16, kernel_size=3, stride=1, padding=1),
-            self.conv_block(c_in=16, c_out=16, kernel_size=3, stride=1, padding=1),
             self.conv_block(c_in=16, c_out=32, kernel_size=3, stride=1, padding=1),
-        )
-
-        self.block3 = nn.Sequential(
-            self.conv_block(c_in=32, c_out=32, kernel_size=3, stride=1, padding=1),
-            self.conv_block(c_in=32, c_out=32, kernel_size=3, stride=1, padding=1),
-            self.conv_block(c_in=32, c_out=32, kernel_size=3, stride=1, padding=1),
-            self.conv_block(c_in=32, c_out=32, kernel_size=3, stride=1, padding=1),
             self.conv_block(c_in=32, c_out=64, kernel_size=3, stride=1, padding=1),
-        )
-
-        self.block4 = nn.Sequential(
-            self.conv_block(c_in=64, c_out=64, kernel_size=3, stride=1, padding=1),
-            self.conv_block(c_in=64, c_out=64, kernel_size=3, stride=1, padding=1),
-            self.conv_block(c_in=64, c_out=64, kernel_size=3, stride=1, padding=1),
             self.conv_block(c_in=64, c_out=64, kernel_size=3, stride=1, padding=1),
             self.conv_block(c_in=64, c_out=128, kernel_size=3, stride=1, padding=1),
         )
 
-        self.fc1 = nn.Linear(in_features=8 * 8 * 128, out_features=64)
-        self.fc2 = nn.Linear(in_features=64, out_features=num_classes)
+        self.fc1 = nn.Linear(
+            in_features=4 * 4 * 128, out_features=self.config.latent_emb_size
+        )
+        self.fc2 = nn.Linear(
+            in_features=self.config.latent_emb_size, out_features=num_classes
+        )
 
-        self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
+    def conv_block(self, c_in, c_out, **kwargs):
+        return nn.Sequential(
+            nn.Conv2d(in_channels=c_in, out_channels=c_out, **kwargs),
+            nn.BatchNorm2d(num_features=c_out),
+            nn.ReLU(),
+            nn.Dropout2d(p=0.2),
+            nn.MaxPool2d(2, 2),
+        )
 
     def forward(self, x):
-        x = self.block1(x)
-        x = self.maxpool(x)
-
-        x = self.block2(x)
-        x = self.maxpool(x)
-
-        x = self.block3(x)
-        x = self.maxpool(x)
-
-        x = self.block4(x)
-        x = self.maxpool(x)
-
+        x = self.feature_extractor(x)
         x = x.flatten(start_dim=1)
         x = self.fc1(x)
         x = self.fc2(x)
@@ -74,30 +53,11 @@ class StimulusClassifier(nn.Module):
         return best_y_idx
 
     def get_latent_rep(self, x):
-        x = self.block1(x)
-        x = self.maxpool(x)
-
-        x = self.block2(x)
-        x = self.maxpool(x)
-
-        x = self.block3(x)
-        x = self.maxpool(x)
-
-        x = self.block4(x)
-        x = self.maxpool(x)
-
+        x = self.feature_extractor(x)
         x = x.flatten(start_dim=1)
         x = self.fc1(x)
 
         return x
-
-    def conv_block(self, c_in, c_out, **kwargs):
-        return nn.Sequential(
-            nn.Conv2d(in_channels=c_in, out_channels=c_out, **kwargs),
-            nn.BatchNorm2d(num_features=c_out),
-            nn.ReLU(),
-            nn.Dropout2d(p=0.5),
-        )
 
 
 class FMRIClassifier(nn.Module):
@@ -106,21 +66,22 @@ class FMRIClassifier(nn.Module):
     def __init__(self, num_features, num_classes):
         super(FMRIClassifier, self).__init__()
 
-        self.block_1 = self.lin_block(f_in=num_features, f_out=64)
-        self.block_2 = self.lin_block(f_in=64, f_out=num_classes)
-
-    def forward(self, x):
-        x = self.block_1(x)
-        x = self.block_2(x)
-
-        return x
+        self.config = FMRIClassifierConfig()
+        self.fc1 = self.lin_block(f_in=num_features, f_out=self.config.latent_emb_size)
+        self.fc2 = self.lin_block(f_in=self.config.latent_emb_size, f_out=num_classes)
 
     def lin_block(self, f_in, f_out):
         return nn.Sequential(
             nn.Linear(in_features=f_in, out_features=f_out),
             nn.ReLU(),
-            nn.Dropout2d(p=0.5),
+            nn.Dropout(p=0.7),
         )
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.fc2(x)
+
+        return x
 
     def predict(self, x):
         y = self.forward(x)
@@ -130,7 +91,7 @@ class FMRIClassifier(nn.Module):
         return best_y_idx
 
     def get_latent_rep(self, x):
-        x = self.block_1(x)
+        x = self.fc1(x)
 
         return x
 
